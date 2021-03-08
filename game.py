291d90fe4e9ebafe4b7e401a69baf4b46401df4c
archy-co <yaroslav.moskalyk@ucu.edu.ua>
2021-03-08 16:53:45 +0200

    Initial code upload

---
+++ game.py
@@ -0,0 +1,89 @@
+global defeated_num
+defeated_num = 0
+
+class Room:
+    def __init__(self, name):
+        self.name = name
+        self.linked_rooms = []
+        self.character = None
+        self.item = None
+
+    def set_description(self, description):
+        self.description = description
+
+    def link_room(self, room, side):
+        self.linked_rooms.append([room, side])
+
+    def set_character(self, character):
+        self.character = character
+
+    def set_item(self, item):
+        self.item = item
+
+    def get_item(self):
+        return self.item
+
+    def get_details(self):
+        print(self.name, '-', self.description)
+
+    def move(self, command):
+        for linked_room, side in self.linked_rooms:
+            if side == command:
+                return linked_room
+        return self
+
+    def get_character(self):
+        return self.character
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def set_description(self, description):
+        self.description = description
+
+    def describe(self):
+        print(self.name, '-', self.description)
+
+    def get_name(self):
+        return self.name
+
+
+class Character:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def describe(self):
+        print(self.name, '-', self.description)
+
+    def talk(self):
+        print(self.conversation)
+
+
+class Enemy(Character):
+    def __init__(self, *attrs):
+        super().__init__(*attrs)
+
+    def set_conversation(self, conversation):
+        self.conversation = conversation
+    
+    def set_weakness(self, weakness):
+        self.weakness = weakness
+
+    def fight(self, fight_with):
+        if self.weakness == fight_with:
+            global defeated_num
+            print('defeated here:', defeated_num)
+            defeated_num += 1
+            return True
+        return False
+
+    def get_defeated(self):
+        return defeated_num
+
+
+class Friend(Character):
+    pass
+
